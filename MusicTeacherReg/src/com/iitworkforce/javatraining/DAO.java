b9cf635f1f43c00b192fa8daab71939304a1deb2
javarishi <rishi.h2kinfosys@gmail.com>
2016-12-27 18:18:18 +0530

    Code Comments

--- MusicTeacherReg/src/com/iitworkforce/javatraining/DAO.java
+++ MusicTeacherReg/src/com/iitworkforce/javatraining/DAO.java
@@ -5,21 +5,24 @@ import com.iitworkforce.javatraining.MusicTeacher;
 
 public  class DAO
 {
-	static Connection conn = null;
+	private static Connection conn = null;
 	
 	public static Connection getDAO()
 	{
-			
+		// Connection String can be instance variable - private static final String 
 	try
 	{
 		String URL = "jdbc:mysql://localhost/javaproject";
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.jdbc.Driver"); // Declare outside
 		conn= DriverManager.getConnection(URL,"root","aarush10");
+		// Also try JNDI approach. I will give you video for JNDI
 	}
 	catch(Exception e)
 	{
-		
+		// Exception handling - do you need Video for this?
 	}
 	return conn;
 	}
+	
+	// Add a method to close the connection
 }
